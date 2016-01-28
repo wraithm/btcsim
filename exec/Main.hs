6654eac08e52c7bbcd69bf62a50f690872d825f0
Matthew Wraith <wraithm@gmail.com>
2016-01-28 02:51:01 -0600

    Initial commit

---
+++ exec/Main.hs
@@ -0,0 +1,35 @@
+module Main where
+
+import           Data.Blockchain
+
+import           GHC.Word        (Word32)
+
+import           System.Random
+
+
+randomBlockchainR :: Random a => (a, a) -> Word32 -> Blockchain a -> IO (Blockchain a)
+randomBlockchainR _ 0 bc = return bc
+randomBlockchainR r n [] = do
+    gb <- mineBlock initialTarget zeroHash =<< randomRIO r
+    randomBlockchainR r (n - 1) [gb]
+randomBlockchainR r n (x:bc) = do
+    b <- mineBlock initialTarget (blockHash x) =<< randomRIO r
+    randomBlockchainR r (n - 1) (b:x:bc)
+
+
+randomBlockchain :: (Bounded a, Random a) => Word32 -> Blockchain a -> IO (Blockchain a)
+randomBlockchain = randomBlockchainR (minBound, maxBound)
+
+
+randomWord32BlockChain :: Word32 -> IO (Blockchain Word32)
+randomWord32BlockChain n = do
+    gb <- genesisBlock
+    randomBlockchainR (0,1000000) (n - 1) [gb]
+
+
+main :: IO ()
+main = do
+    bc <- randomWord32BlockChain 10
+    putStrLn $ unlines $ reverse $ map show bc
+
+    print (valid (const True) bc)
---
+++ src/Data/Blockchain.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module Data.Blockchain where
+
+import qualified Crypto.Hash.SHA256     as SHA256
+
+import           Data.Binary
+import           Data.Binary.Put        (putByteString, putWord32le, runPut)
+import           Data.ByteString
+import qualified Data.ByteString.Base16 as B16
+import qualified Data.ByteString.Char8  as C8
+import           Data.ByteString.Lazy   (toStrict)
+
+import           System.Random          (randomIO)
+
+
+class Hashable a where
+    putHash :: a -> Put
+
+
+instance Hashable ByteString where
+    putHash = put
+
+
+newtype Hash256 = Hash256 ByteString
+    deriving (Eq, Ord, Binary)
+
+
+instance Hashable Hash256 where
+    putHash (Hash256 bs) = putByteString bs
+
+
+instance Show Hash256 where
+    show (Hash256 bs) = C8.unpack (B16.encode bs)
+
+
+initialTarget :: Hash256
+initialTarget =
+    Hash256 (pack (Prelude.take 8 (allZeros ++ allOnes)))
+  where
+    allOnes = Prelude.repeat maxBound
+    allZeros = Prelude.replicate 2 minBound
+
+
+zeroHash :: Hash256
+zeroHash = Hash256 (pack (Prelude.replicate 8 minBound))
+
+
+sha256 :: Hashable a => a -> Hash256
+sha256 = Hash256 . SHA256.hash . toStrict . runPut . putHash
+
+
+data Block a = Block
+    { blockHeader :: BlockHeader
+    , body        :: a
+    } deriving (Show, Eq)
+
+
+blockHash :: Block a -> Hash256
+blockHash = sha256 . blockHeader
+
+
+data BlockHeader = BlockHeader
+    { prevHash :: Hash256
+    , nonce    :: Word32
+    } deriving (Show, Eq)
+
+
+instance Hashable BlockHeader where
+    putHash (BlockHeader p n) =
+        put p >> putWord32le n
+
+
+type Blockchain a = [Block a]
+
+
+valid :: (Block a -> Bool) -> Blockchain a -> Bool
+valid p [x] = p x && blockSolution initialTarget (blockHeader x)
+valid p (x:y:bs)
+    | p x && blockSolution initialTarget (blockHeader x) && prevHash (blockHeader x) == blockHash y =
+        valid p (y:bs)
+valid _ _ = False
+
+
+mineBlock :: Hash256 -> Hash256 -> a -> IO (Block a)
+mineBlock target ph x = do
+    n <- randomIO
+    let trialHeader = BlockHeader ph n
+    if blockSolution target trialHeader
+        then return $ Block trialHeader x
+        else mineBlock target ph x
+
+
+blockSolution :: Hash256 -> BlockHeader -> Bool
+blockSolution target trialHeader =
+    sha256 (sha256 trialHeader) < target
+
+
+genesisBlock :: IO (Block Word32)
+genesisBlock = mineBlock initialTarget zeroHash 0
